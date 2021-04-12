1b7e2eb066f8de43586023a5d7bc757d987d89ba
ashukv12 <82150072+ashukv12@users.noreply.github.com>
2021-04-12 11:08:22 +0530

    Added files to run the starter program locally

---
+++ first-program/main.go
@@ -0,0 +1,13 @@
+package main
+
+/*
+This is a multi-line comment.
+import keyword makes another package available
+  for this .go "file".
+import "fmt" lets you access fmt package's functionality
+  here in this file.
+*/
+import "fmt"
+func main() {
+	fmt.Println("Hello Gopher!")
+}
