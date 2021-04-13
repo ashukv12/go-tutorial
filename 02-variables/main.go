10e11429d9c095f7c7670008bea3b339382b192c
ashukv12 <82150072+ashukv12@users.noreply.github.com>
2021-04-13 16:02:24 +0530

    Added files to show use of variables

---
+++ 02-variables/main.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	// Short variable declaration syntax
+	name := "Rajeev Singh"
+	age, salary, isProgrammer := 35, 50000.0, true
+
+	fmt.Println(name, age, salary, isProgrammer)
+}
